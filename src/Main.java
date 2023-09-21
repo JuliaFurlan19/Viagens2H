ce2975d088b564b3897539d7b7c4f798bfc7fdb2
Júlia Furlan <julialeitefurlan@gmail.com>
2023-09-21 10:56:22 -0300

    Novo objeto

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
         var mochileiro = new Turista();
         mochileiro.setNome("Lohan");
-        mochileiro.setCpf(Validacao.cpf("73527958"));
 
         var mochileira = new Turista("Eliza");
-        mochileira.setCpf(Validacao.cpf("794782750"));
 
         String retorno = mochileira.viajar();
         System.out.println(retorno);
+
+        var outroTurista = new Turista();
+        String nome = JOptionPane.showInputDialog("Entre com seu nome: ");
+        outroTurista.setNome(nome);
+        JOptionPane.showMessageDialog(null, "O nome digitado foi: " + outroTurista.getNome());
+
+        String cpf = JOptionPane.showInputDialog("Entre com seu CPF: ");
+        if(Validacao.cpf(cpf)){
+            outroTurista.setCpf(cpf);
+        } else {
+            System.out.println("CPF não validado!");
+        }
+
+        JOptionPane.showMessageDialog(null, outroTurista.getCpf() + "validado!");
+
     }
 }
